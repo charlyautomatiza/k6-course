2feb539988ae33e654898907d5cb73b0ce9698fd
charlyautomatiza <charlyautomatiza@gmail.com>
2023-12-11 19:59:14 -0300

    first commit

---
+++ src/browser.js
@@ -0,0 +1,40 @@
+// Para tener un browser en primer plano -> K6_BROWSER_HEADLESS=false
+import { browser } from 'k6/experimental/browser';
+import { check } from 'k6';
+
+export const options = {
+  scenarios: {
+    ui: {
+      executor: 'shared-iterations',
+      options: {
+        browser: {
+          type: 'chromium',
+        },
+      },
+    },
+  },
+  thresholds: {
+    checks: ["rate==1.0"]
+  }
+}
+
+export default async function () {
+  const page = browser.newPage();
+
+  try {
+    await page.goto('https://test.k6.io/my_messages.php');
+
+    page.locator('input[name="login"]').type('admin');
+    page.locator('input[name="password"]').type('123');
+
+    const submitButton = page.locator('input[type="submit"]');
+
+    await Promise.all([page.waitForNavigation(), submitButton.click()]);
+
+    check(page, {
+      'header': p => p.locator('h2').textContent() == 'Welcome, admin!',
+    });
+  } finally {
+    page.close();
+  }
+}
